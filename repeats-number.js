d0ee88e1dd3daf02cb83134f0dccd23140b95356
hareeshcs33 <hareeshcs33@gmail.com>
2021-05-06 09:23:48 +0530

    second value

---
+++ repeats-number.js
@@ -0,0 +1,16 @@
+console.log("Repeats number");
+
+//return number with most repeats
+//if two numbers the epeated the same return first
+//if none repeated return -1
+
+function testRepeat(arr){
+   return arr;
+}
+
+console.log(testRepeat([5,2,2,1,5]));
+//return 5
+// console.log(testRepeat([6,5,5,10,10,10]));
+//return 10
+// console.log(testRepeat([3,4,1,6,10]));
+//return -1
